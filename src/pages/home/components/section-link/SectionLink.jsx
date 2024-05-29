7afa8cf4f1aec9e971184406b7d02dbb45d06ce1
hazimdanishhh <hazimdanishhh@gmail.com>
2024-05-29 13:16:13 +0800

    Slight component modifications

--- src/pages/home/components/section-link/SectionLink.jsx
+++ src/pages/home/components/section-link/SectionLink.jsx
@@ -1,28 +1,43 @@
-import React from 'react';
+import React from "react";
 import "./sectionLink.scss";
 import { Link } from "react-router-dom";
 
 function SectionLink() {
   return (
-    <div className='background'>
-      <div className='bg-gradient'>
-        <Link to="/about" className='section-link-div'>
-          <div className='section-link-wrapper'>
-            <div className='text'>
-              <h2 className='title'>About Us</h2>
+    <div className="background">
+      <div className="bg-gradient">
+        <Link to="/about" className="section-link-div">
+          <div className="section-link-wrapper">
+            <div className="text">
+              <h2 className="title">About Us</h2>
 
-              <p className='desc'>Incorporated in Malaysia in 1991, Hyrax Oil Sdn Bhd &#40;Hyrax Oil&#41; has achieved a remarkable growth since its inception.</p>
-              <p className='desc'>With {new Date().getFullYear() - 1991} years of experience, Hyrax Oil has rapidly grown from its humble beginning as a transformer oils trading company to become an established manufacturer and a global player in the manufacturing of lubricants with a full range of top quality and high-performance lubricants; including transformer oils, specialty products and other petroleum derivatives.</p>
+              <p className="desc">
+                Incorporated in Malaysia in 1991, Hyrax Oil Sdn Bhd &#40;Hyrax
+                Oil&#41; has achieved a remarkable growth since its inception.
+              </p>
+              <p className="desc">
+                With {new Date().getFullYear() - 1991} years of experience,
+                Hyrax Oil has rapidly grown from its humble beginning as a
+                transformer oils trading company to become an established
+                manufacturer and a global player in the manufacturing of
+                lubricants with a full range of top quality and high-performance
+                lubricants; including transformer oils, specialty products and
+                other petroleum derivatives.
+              </p>
             </div>
-            <div className='link-div'>
-              <span className='link'>read more</span>
-              <img className='link-arrow' src="./link-arrow.svg" alt="Link Arrow" />
+            <div className="link-div">
+              <span className="link">read more</span>
+              <img
+                className="link-arrow"
+                src="./link-arrow.svg"
+                alt="Link Arrow"
+              />
             </div>
           </div>
         </Link>
       </div>
     </div>
-  )
+  );
 }
 
-export default SectionLink
+export default SectionLink;
--- src/pages/home/components/trust-our-quality/TrustQuality.jsx
+++ src/pages/home/components/trust-our-quality/TrustQuality.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./trustQuality.scss";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 function TrustQuality() {
   return (
     <div className="trust-background">
       <div className="bg-gradient">
         <div className="section-link-wrapper">
-          <div className="text">
+          <motion.div
+            className="text"
+            initial={{ opacity: 0, x: -600 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.3 }}
+          >
             <h2 className="title">
               Trust Our <span>Quality</span>
             </h2>
@@ -22,7 +28,7 @@ function TrustQuality() {
               href="https://www.api.org/"
               target="__blank"
             >
-              American Petroleum Institute &#40;“API”&#41;
+              American Petroleum Institute &#40;API&#41;
               <img
                 className="link-arrow"
                 src="./link-arrow-2.svg"
@@ -34,7 +40,7 @@ function TrustQuality() {
               href="https://www.acea.auto/"
               target="__blank"
             >
-              European Automobile Manufacturers Association &#40;“ACEA”&#41;
+              European Automobile Manufacturers Association &#40;ACEA&#41;
               <img
                 className="link-arrow"
                 src="./link-arrow-2.svg"
@@ -46,7 +52,7 @@ function TrustQuality() {
               href="https://www.mtu-solutions.com/eu/en.regionselect.html"
               target="__blank"
             >
-              MTU Friedrichshafen GmbH &#40;"MTU"&#41;
+              MTU Friedrichshafen GmbH &#40;MTU&#41;
               <img
                 className="link-arrow"
                 src="./link-arrow-2.svg"
@@ -58,7 +64,7 @@ function TrustQuality() {
               href="https://www.doble.com/"
               target="__blank"
             >
-              Doble Engineering Company &#40;“DOBLE”&#41;
+              Doble Engineering Company &#40;DOBLE&#41;
               <img
                 className="link-arrow"
                 src="./link-arrow-2.svg"
@@ -66,7 +72,7 @@ function TrustQuality() {
               />
             </a>
             <img className="api-qr" src="./API-QR.png" alt="API QR Code" />
-          </div>
+          </motion.div>
 
           <Link
             className="link-div"

de25fd85fec78d57b0e087093813fa951ec1be85
hazimdanishhh <hazimdanishhh@gmail.com>
2024-12-20 16:07:34 +0800

    Modify and enhance navbar for desktop and mobile

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -68,225 +68,269 @@ function Layout() {
 
   return (
     <>
-      <header className="navbar">
-        <nav className={`${isActive ? "active" : ""}`}>
-          {/* LOGO */}
-          <Link to="/">
-            <img
-              loading="lazy"
-              className="nav-logo"
-              src="./hyraxoil33.png"
-              alt="Hyrax Oil Logo"
-            />
-          </Link>
+      <nav className={`${isActive ? "active" : ""}`}>
+        {/* LOGO */}
+        <Link to="/">
+          <img
+            loading="lazy"
+            className="nav-logo"
+            src="./hyraxoil33.png"
+            alt="Hyrax Oil Logo"
+          />
+        </Link>
 
-          {/* NAVIGATION LINKS */}
-          <ul className={`nav-list ${isActive ? "active" : ""}`}>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
+        {/* NAVIGATION LINKS */}
+        <ul className={`nav-list ${isActive ? "active" : ""}`}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
 
-            <li
-              onClick={() => openOverlay("about")}
-              className={`${activePopup === "about" ? "link-active" : ""}`}
-            >
-              <Link to="#">About</Link>
-            </li>
-
-            <li
-              onClick={() => openOverlay("products")}
-              className={`${activePopup === "products" ? "link-active" : ""}`}
-            >
-              <Link to="#">Products</Link>
-            </li>
-
-            <li
-              onClick={() => openOverlay("services")}
-              className={`${activePopup === "services" ? "link-active" : ""}`}
-            >
-              <Link to="#">Services</Link>
-            </li>
-
-            <li
-              onClick={() => openOverlay("media")}
-              className={`${activePopup === "media" ? "link-active" : ""}`}
-            >
-              <Link to="#">Media</Link>
-            </li>
-          </ul>
-
-          {/* NAVIGATION BUTTONS */}
-          <div className={`nav-buttons ${isActive ? "active" : ""}`}>
-            <Link className="button-type-2" to="/careers">
-              Careers
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="14"
-                height="12"
-                viewBox="0 0 14 12"
-                fill="none"
-              >
-                <path
-                  d="M13 6L7.85714 1M13 6L7.85714 11M13 6H1"
-                  stroke="#f26f27"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </Link>
-            <Link className="button-type-1" to="/contact">
-              Contact Us
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="14"
-                height="12"
-                viewBox="0 0 14 12"
-                fill="none"
-              >
-                <path
-                  d="M13 6L7.85714 1M13 6L7.85714 11M13 6H1"
-                  stroke="white"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </Link>
-          </div>
-
-          {/* MOBILE NAVIGATION */}
-          <button
-            className={`hamburger-icon ${isActive ? "active" : ""}`}
-            onClick={toggleMenu}
-            aria-label="Toggle navigation"
+          <li
+            onClick={() => openOverlay("about")}
+            className={`${activePopup === "about" ? "link-active" : ""}`}
           >
+            <Link to="#">About</Link>
+          </li>
+
+          <li
+            onClick={() => openOverlay("products")}
+            className={`${activePopup === "products" ? "link-active" : ""}`}
+          >
+            <Link to="#">Products</Link>
+          </li>
+
+          <li
+            onClick={() => openOverlay("services")}
+            className={`${activePopup === "services" ? "link-active" : ""}`}
+          >
+            <Link to="#">Services</Link>
+          </li>
+
+          <li
+            onClick={() => openOverlay("media")}
+            className={`${activePopup === "media" ? "link-active" : ""}`}
+          >
+            <Link to="#">Media</Link>
+          </li>
+        </ul>
+
+        {/* NAVIGATION BUTTONS */}
+        <div className={`nav-buttons ${isActive ? "active" : ""}`}>
+          <Link className="button-type-2" to="/careers">
+            Careers
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 100 70"
-              width="40"
-              height="40"
-              className="hamburger-svg"
+              width="14"
+              height="12"
+              viewBox="0 0 14 12"
+              fill="none"
             >
-              <rect className="line top" width="100" height="10" rx="5"></rect>
-              <rect
-                className="line middle"
-                y="30"
-                width="100"
-                height="10"
-                rx="5"
-              ></rect>
-              <rect
-                className="line bottom"
-                y="60"
-                width="100"
-                height="10"
-                rx="5"
-              ></rect>
+              <path
+                d="M13 6L7.85714 1M13 6L7.85714 11M13 6H1"
+                stroke="#f26f27"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
             </svg>
-          </button>
-        </nav>
-
-        {/* NAV OVERLAY DESKTOP*/}
-        {activePopup && (
-          <div className="nav-outer" onClick={closeOverlay}>
-            <div
-              className="nav-overlay"
-              onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside the modal
+          </Link>
+          <Link className="button-type-1" to="/contact">
+            Contact Us
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="12"
+              viewBox="0 0 14 12"
+              fill="none"
             >
-              {activePopup === "about" && (
-                <>
-                  <div className="nav-card-layout">
-                    {aboutSections.map((segment, index) => (
-                      <NavCard
-                        key={index}
-                        src={segment.src}
-                        alt={segment.alt}
-                        title={segment.title}
-                        link={segment.link}
-                      />
-                    ))}
-                  </div>
-                </>
-              )}
-              {activePopup === "products" && (
-                <>
-                  <div className="nav-overlay-logos">
-                    <img
-                      loading="lazy"
-                      src="./hyrax-logo.png"
-                      alt="hyrax brand logo"
-                      className="nav-overlay-logo"
+              <path
+                d="M13 6L7.85714 1M13 6L7.85714 11M13 6H1"
+                stroke="white"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </Link>
+        </div>
+
+        {/* MOBILE NAVIGATION */}
+        <button
+          className={`hamburger-icon ${isActive ? "active" : ""}`}
+          onClick={toggleMenu}
+          aria-label="Toggle navigation"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 100 70"
+            width="40"
+            height="40"
+            className="hamburger-svg"
+          >
+            <rect className="line top" width="100" height="10" rx="5"></rect>
+            <rect
+              className="line middle"
+              y="30"
+              width="100"
+              height="10"
+              rx="5"
+            ></rect>
+            <rect
+              className="line bottom"
+              y="60"
+              width="100"
+              height="10"
+              rx="5"
+            ></rect>
+          </svg>
+        </button>
+      </nav>
+
+      {/* NAV OVERLAY DESKTOP*/}
+      {activePopup && (
+        <>
+          <div className="nav-outer" onClick={closeOverlay}></div>
+
+          <div
+            className="nav-overlay"
+            onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside the modal
+          >
+            {activePopup === "about" && (
+              <>
+                <div className="nav-overlay-header">
+                  <svg
+                    onClick={closeOverlay}
+                    className="back-icon"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m15 19-7-7 7-7"
                     />
-                    <img
-                      loading="lazy"
-                      src="./api.png"
-                      alt="American Petroleum Institute Logo"
-                      className="nav-overlay-logo"
+                  </svg>
+
+                  <h2>About</h2>
+                </div>
+                <div className="nav-card-layout">
+                  {aboutSections.map((segment, index) => (
+                    <NavCard
+                      key={index}
+                      src={segment.src}
+                      alt={segment.alt}
+                      title={segment.title}
+                      link={segment.link}
                     />
-                    <img
-                      loading="lazy"
-                      src="./malaysian-brand.png"
-                      alt="Malaysian Brand Logo"
-                      className="nav-overlay-logo"
+                  ))}
+                </div>
+              </>
+            )}
+            {activePopup === "products" && (
+              <>
+                <div className="nav-overlay-header">
+                  <svg
+                    onClick={closeOverlay}
+                    className="back-icon"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m15 19-7-7 7-7"
                     />
-                  </div>
-                  <div className="nav-card-layout">
-                    {productSegments.map((segment, index) => (
-                      <NavCard
-                        key={index}
-                        src={segment.src}
-                        alt={segment.alt}
-                        title={segment.title}
-                        link={segment.link}
-                      />
-                    ))}
-                  </div>
-                  <div className="nav-card-layout">
-                    {productSegments2.map((segment, index) => (
-                      <NavCard
-                        key={index}
-                        src={segment.src}
-                        alt={segment.alt}
-                        title={segment.title}
-                        link={segment.link}
-                      />
-                    ))}
-                  </div>
-                </>
-              )}
-              {activePopup === "services" && (
-                <>
-                  <div className="nav-card-layout">
-                    {servicesSections.map((segment, index) => (
-                      <NavCard
-                        key={index}
-                        src={segment.src}
-                        alt={segment.alt}
-                        title={segment.title}
-                        link={segment.link}
-                      />
-                    ))}
-                  </div>
-                </>
-              )}
-              {activePopup === "media" && (
-                <>
-                  <div className="nav-card-layout">
-                    {mediaSections.map((segment, index) => (
-                      <NavCard
-                        key={index}
-                        src={segment.src}
-                        alt={segment.alt}
-                        title={segment.title}
-                        link={segment.link}
-                      />
-                    ))}
-                  </div>
-                </>
-              )}
-            </div>
+                  </svg>
+
+                  <h2>Products</h2>
+                </div>
+                <div className="nav-overlay-logos">
+                  <img
+                    loading="lazy"
+                    src="./hyrax-logo.png"
+                    alt="hyrax brand logo"
+                    className="nav-overlay-logo"
+                  />
+                  <img
+                    loading="lazy"
+                    src="./api.png"
+                    alt="American Petroleum Institute Logo"
+                    className="nav-overlay-logo"
+                  />
+                  <img
+                    loading="lazy"
+                    src="./malaysian-brand.png"
+                    alt="Malaysian Brand Logo"
+                    className="nav-overlay-logo"
+                  />
+                </div>
+                <div className="nav-card-layout">
+                  {productSegments.map((segment, index) => (
+                    <NavCard
+                      key={index}
+                      src={segment.src}
+                      alt={segment.alt}
+                      title={segment.title}
+                      link={segment.link}
+                    />
+                  ))}
+                </div>
+                {/* <div className="nav-card-layout">
+                  {productSegments2.map((segment, index) => (
+                    <NavCard
+                      key={index}
+                      src={segment.src}
+                      alt={segment.alt}
+                      title={segment.title}
+                      link={segment.link}
+                    />
+                  ))}
+                </div> */}
+              </>
+            )}
+            {activePopup === "services" && (
+              <>
+                <div className="nav-card-layout">
+                  {servicesSections.map((segment, index) => (
+                    <NavCard
+                      key={index}
+                      src={segment.src}
+                      alt={segment.alt}
+                      title={segment.title}
+                      link={segment.link}
+                    />
+                  ))}
+                </div>
+              </>
+            )}
+            {activePopup === "media" && (
+              <>
+                <div className="nav-card-layout">
+                  {mediaSections.map((segment, index) => (
+                    <NavCard
+                      key={index}
+                      src={segment.src}
+                      alt={segment.alt}
+                      title={segment.title}
+                      link={segment.link}
+                    />
+                  ))}
+                </div>
+              </>
+            )}
           </div>
-        )}
-      </header>
+        </>
+      )}
 
       <Outlet />
     </>
--- src/pages/navbar/components/nav-products/NavProductSegments.js
+++ src/pages/navbar/components/nav-products/NavProductSegments.js
@@ -41,4 +41,46 @@ export const productSegments = [
     title: "Marine & Large Engine Oils",
     link: "#",
   },
+  {
+    src: "./navbar/gear.png",
+    alt: "Gear Oils",
+    title: "Gear Oils",
+    link: "#",
+  },
+  {
+    src: "./navbar/transmission.png",
+    alt: "Transmission Fluids",
+    title: "Transmission Fluids",
+    link: "#",
+  },
+  {
+    src: "./navbar/industrial.png",
+    alt: "Industrial Oils",
+    title: "Industrial Oils",
+    link: "#",
+  },
+  {
+    src: "./navbar/transformer.png",
+    alt: "Transformer & Electrical Oils",
+    title: "Transformer & Electrical Oils",
+    link: "#",
+  },
+  {
+    src: "./navbar/specialty.png",
+    alt: "Specialty Oils",
+    title: "Specialty Oils",
+    link: "#",
+  },
+  {
+    src: "./navbar/brake.png",
+    alt: "Brake Fluids",
+    title: "Brake Fluids",
+    link: "#",
+  },
+  {
+    src: "./navbar/grease.png",
+    alt: "Greases",
+    title: "Greases",
+    link: "#",
+  },
 ];

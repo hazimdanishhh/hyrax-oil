ea59a14a5a8a003ddeb62d07bc7decae9e971eb9
hazimdanishhh <hazimdanishhh@gmail.com>
2025-01-03 12:26:24 +0800

    Add Google Maps link in Footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,106 +1,123 @@
 import React from "react";
 import "./footer.scss";
 import { motion } from "framer-motion";
+import { Outlet, Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className="footer">
-      <div className="footer-wrapper">
-        <div className="footer-segments">
-          <div className="footer-item">
-            <h3>MENU</h3>
-            <a href="#">Our Products</a>
-            <a href="#">Our Quality</a>
-            <a href="#">Market</a>
-            <a href="#">About Us</a>
-            <a href="#">Our Leaders</a>
-            <a href="#">Services</a>
-            <a href="#">Blending Plants</a>
-            <a href="#">Info</a>
+    <>
+      <div className="footer">
+        <div className="footer-wrapper">
+          <div className="footer-segments">
+            <div className="footer-item">
+              <h3>MENU</h3>
+              <a href="#">Our Products</a>
+              <a href="#">Our Quality</a>
+              <a href="#">Market</a>
+              <a href="#">About Us</a>
+              <a href="#">Our Leaders</a>
+              <a href="#">Services</a>
+              <a href="#">Blending Plants</a>
+              <a href="#">Info</a>
+            </div>
+
+            <div className="footer-item">
+              <h3>HEAD OFFICE</h3>
+              <Link
+                to="https://www.google.com/maps/place/Hyrax+Oil+Sdn.+Bhd./data=!4m2!3m1!1s0x0:0x53085859f88dd00d?sa=X&ved=1t:2428&ictx=111"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                A-8-1, Megan Avenue II,
+                <br />
+                12, Jalan Yap Kwan Seng,
+                <br />
+                Kampung Baru,
+                <br />
+                50450 Kuala Lumpur,
+                <br />
+                Wilayah Persekutuan Kuala Lumpur.
+              </Link>
+            </div>
+
+            <div className="footer-item">
+              <h3>CONTACT US</h3>
+              <span>
+                <a href="tel:+60321635893">Tel: &#40;+603&#41; 2163-5893</a>
+              </span>
+              <span>
+                <a href="tel:+60321646577">Fax: &#40;+603&#41; 2164-6577</a>
+              </span>
+              <span>
+                <a href="mailto:hyrax@hyraxoil.com">
+                  Email: hyrax@hyraxoil.com
+                </a>
+              </span>
+            </div>
+
+            <div className="footer-socials">
+              <a href="https://www.linkedin.com/company/hyrax-oil-sdn-bhd/">
+                <motion.img
+                  className="socials-img"
+                  src="linkedin.svg"
+                  alt="Linked In Logo"
+                  initial={{ scale: 1 }}
+                  whileHover={{ scale: 1.05 }}
+                />
+              </a>
+              <a href="https://www.linkedin.com/company/hyrax-oil-sdn-bhd/">
+                <motion.img
+                  className="socials-img"
+                  src="facebook.svg"
+                  alt="Facebook Logo"
+                  initial={{ scale: 1 }}
+                  whileHover={{ scale: 1.05 }}
+                />
+              </a>
+              <a href="https://www.linkedin.com/company/hyrax-oil-sdn-bhd/">
+                <motion.img
+                  className="socials-img"
+                  src="instagram.svg"
+                  alt="Instagram Logo"
+                  initial={{ scale: 1 }}
+                  whileHover={{ scale: 1.05 }}
+                />
+              </a>
+              <a href="https://www.linkedin.com/company/hyrax-oil-sdn-bhd/">
+                <motion.img
+                  className="socials-img"
+                  src="youtube.svg"
+                  alt="Youtube Logo"
+                  initial={{ scale: 1 }}
+                  whileHover={{ scale: 1.05 }}
+                />
+              </a>
+            </div>
           </div>
 
-          <div className="footer-item">
-            <h3>HEAD OFFICE</h3>
-            <span>A-8-1, Megan Avenue II,</span>
-            <span>12, Jalan Yap Kwan Seng,</span>
-            <span>Kampung Baru,</span>
-            <span>50450 Kuala Lumpur,</span>
-            <span>Wilayah Persekutuan Kuala Lumpur.</span>
-          </div>
-
-          <div className="footer-item">
-            <h3>CONTACT US</h3>
+          <div className="footer-copyright">
             <span>
-              <a href="tel:+60321635893">Tel: &#40;+603&#41; 2163-5893</a>
+              &copy; {new Date().getFullYear()} Hyrax Oil Sdn Bhd 199101017905
+              &#40;228216-K&#41; &#124; ALL RIGHTS RESERVED
             </span>
-            <span>
-              <a href="tel:+60321646577">Fax: &#40;+603&#41; 2164-6577</a>
-            </span>
-            <span>
-              <a href="mailto:hyrax@hyraxoil.com">Email: hyrax@hyraxoil.com</a>
-            </span>
-          </div>
-
-          <div className="footer-socials">
-            <a href="https://www.linkedin.com/company/hyrax-oil-sdn-bhd/">
-              <motion.img
-                className="socials-img"
-                src="linkedin.svg"
-                alt="Linked In Logo"
-                initial={{ scale: 1 }}
-                whileHover={{ scale: 1.05 }}
+            <div className="footer-logos">
+              <img
+                className="footer-hyrax-logo"
+                src="./hyrax-logo.png"
+                alt="Hyrax Logo"
               />
-            </a>
-            <a href="https://www.linkedin.com/company/hyrax-oil-sdn-bhd/">
-              <motion.img
-                className="socials-img"
-                src="facebook.svg"
-                alt="Facebook Logo"
-                initial={{ scale: 1 }}
-                whileHover={{ scale: 1.05 }}
+              <img
+                className="footer-hyraxoil-logo"
+                src="./hyraxoil-logo.png"
+                alt="Hyrax Oil Logo"
               />
-            </a>
-            <a href="https://www.linkedin.com/company/hyrax-oil-sdn-bhd/">
-              <motion.img
-                className="socials-img"
-                src="instagram.svg"
-                alt="Instagram Logo"
-                initial={{ scale: 1 }}
-                whileHover={{ scale: 1.05 }}
-              />
-            </a>
-            <a href="https://www.linkedin.com/company/hyrax-oil-sdn-bhd/">
-              <motion.img
-                className="socials-img"
-                src="youtube.svg"
-                alt="Youtube Logo"
-                initial={{ scale: 1 }}
-                whileHover={{ scale: 1.05 }}
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className="footer-copyright">
-          <span>
-            &copy; {new Date().getFullYear()} Hyrax Oil Sdn Bhd 199101017905
-            &#40;228216-K&#41; &#124; ALL RIGHTS RESERVED
-          </span>
-          <div className="footer-logos">
-            <img
-              className="footer-hyrax-logo"
-              src="./hyrax-logo.png"
-              alt="Hyrax Logo"
-            />
-            <img
-              className="footer-hyraxoil-logo"
-              src="./hyraxoil-logo.png"
-              alt="Hyrax Oil Logo"
-            />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <Outlet />
+    </>
   );
 }
 

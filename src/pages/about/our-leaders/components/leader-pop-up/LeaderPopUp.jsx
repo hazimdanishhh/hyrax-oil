76b374d0cb7393138ecd43234b87352f79df4437
hazimdanishhh <hazimdanishhh@gmail.com>
2025-01-03 10:56:32 +0800

    Fix Leader Pop Up image sizing

--- src/pages/about/our-leaders/components/leader-pop-up/LeaderPopUp.jsx
+++ src/pages/about/our-leaders/components/leader-pop-up/LeaderPopUp.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import "./LeaderPopUp.scss";
 
@@ -23,56 +23,60 @@ function LeaderPopUp({ leader, onClose }) {
   }, [onClose]);
 
   return (
-    <motion.div
-      className="leader-pop-up"
-      ref={productOverlayRef}
-      initial={{ opacity: 1, x: 200 }}
-      animate={{ opacity: 1, x: 0 }}
-    >
-      <div className="leader-pop-up-content">
-        <svg
-          className="leader-close-icon"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          width="35"
-          height="35"
-          fill="none"
-          viewBox="0 0 24 24"
-          onClick={onClose}
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="1"
-            d="m15 9-6 6m0-6 6 6m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-          />
-        </svg>
-
-        <div className="leader-pop-up-header">
-          <div className="leader-pop-up-img-wrapper">
-            <img
-              loading="lazy"
-              className="leader-pop-up-img"
-              src={leader.src}
-              alt={leader.alt}
+    <AnimatePresence>
+      <motion.div
+        className="leader-pop-up"
+        ref={productOverlayRef}
+        key="modal"
+        initial={{ opacity: 1, x: 200 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: 200 }}
+      >
+        <div className="leader-pop-up-content">
+          <svg
+            className="leader-close-icon"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="35"
+            fill="none"
+            viewBox="0 0 24 24"
+            onClick={onClose}
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1"
+              d="m15 9-6 6m0-6 6 6m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
             />
-          </div>
-          <div className="leader-pop-up-title">
-            <h3>{leader.name}</h3>
-            <h5>{leader.title}</h5>
-          </div>
-        </div>
+          </svg>
 
-        <div className="leader-pop-up-body">
-          {leader.description.map((paragraph, index) => (
-            <p key={index} className="desc-paragraph">
-              {paragraph}
-            </p>
-          ))}
+          <div className="leader-pop-up-header">
+            <div className="leader-pop-up-img-wrapper">
+              <img
+                loading="lazy"
+                className="leader-pop-up-img"
+                src={leader.src}
+                alt={leader.alt}
+              />
+            </div>
+            <div className="leader-pop-up-title">
+              <h3>{leader.name}</h3>
+              <h5>{leader.title}</h5>
+            </div>
+          </div>
+
+          <div className="leader-pop-up-body">
+            {leader.description.map((paragraph, index) => (
+              <p key={index} className="desc-paragraph">
+                {paragraph}
+              </p>
+            ))}
+          </div>
         </div>
-      </div>
-    </motion.div>
+      </motion.div>
+    </AnimatePresence>
   );
 }
 

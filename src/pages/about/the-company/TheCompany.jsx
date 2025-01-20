ee11a44c3142bb03acaa942d258c543128054983
hazimdanishhh <hazimdanishhh@gmail.com>
2025-01-20 11:11:00 +0800

    Create Market Map

--- src/pages/about/the-company/TheCompany.jsx
+++ src/pages/about/the-company/TheCompany.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import "./TheCompany.scss";
 import Layout from "../../Layout";
 import Footer from "../../Footer";
+import Hero from "../../components/hero/Hero";
+import MarketMap from "../../components/market-map/MarketMap";
 
 function TheCompany() {
   return (
     <>
       <Layout />
-      <div>TheCompany</div>
+
+      {/* HERO SECTION */}
+      <Hero
+        image='url("./the-company-background.webp")'
+        crumble="About"
+        crumbleLink="/about/the-company"
+        title="The Company"
+        desc="The Company"
+      />
+
+      <MarketMap />
+
       <Footer />
     </>
   );
